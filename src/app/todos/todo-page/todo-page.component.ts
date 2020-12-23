41ad179ef5ae25bee6848043f9ae032f28ce355f
laulorenz <laulorenz@quilmes.com.ar>
2020-12-23 11:51:53 -0300

    case 41

--- src/app/todos/todo-page/todo-page.component.ts
+++ src/app/todos/todo-page/todo-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: 'app-todo-page',
   templateUrl: './todo-page.component.html',
   styleUrls: ['./todo-page.component.css']
 })
